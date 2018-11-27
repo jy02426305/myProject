1949f7f8bb81246c9d5d287db8aebb1c3bca403c
jy02426305 <88042711@qq.com>
2018-11-28 00:21:13 +0800

    2018.11.28 home2

--- annotationPratice/src/main/java/com/cyx/spring/control/UserControl.java
+++ annotationPratice/src/main/java/com/cyx/spring/control/UserControl.java
@@ -2,9 +2,11 @@ package com.cyx.spring.control;
 
 import com.cyx.spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 
 @Controller(value="userControl")
+@Lazy(value=true) //延迟加载，有使用到UserControl才生成对象（单例）
 public class UserControl {
     @Autowired(required = false)
     private UserService userService;
@@ -14,6 +16,10 @@ public class UserControl {
             userService.sayUser();
             userService.sayOrder();
         }
+    }
+
+    public UserControl(){
+        System.out.println("UserControl 构造函数...");
     }
 
     public void showSelf() {
