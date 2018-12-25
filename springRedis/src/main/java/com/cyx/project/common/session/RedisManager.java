62f38b413ab4c025c3ce6d66a0580276b4f8aa48
jy02426305 <88042711@qq.com>
2018-12-26 01:49:01 +0800

    2018.12.26 shiro

--- springRedis/src/main/java/com/cyx/project/common/session/RedisManager.java
+++ springRedis/src/main/java/com/cyx/project/common/session/RedisManager.java
@@ -1,6 +1,6 @@
 package com.cyx.project.common.session;
 
-import org.apache.shiro.session.Session;
+//import org.apache.shiro.session.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
@@ -10,33 +10,33 @@ import java.util.List;
 
 @Component
 public class RedisManager {
-    @Autowired
-    private RedisTemplate<String, Session> redisTemplate;
-
-    private static final String KEY = "shareSessionMap";
-
-    public void hadd(String sessionId, byte[] bytes){
-        redisTemplate.boundHashOps(KEY).put(sessionId, bytes);
-    }
-    public void hadd(String sessionId, Session session){
-        redisTemplate.boundHashOps(KEY).put(sessionId, session);
-    }
-
-    public void hdelete(String sessionId){
-        redisTemplate.boundHashOps(KEY).delete(sessionId);
-    }
-
-    public Session hget(String sessionId){
-        return (Session) redisTemplate.boundHashOps(KEY).get(sessionId);
-    }
-
-    public List<Session> hmget(){
-        List<Session> list = new ArrayList<>();
-
-        List<Object> values = redisTemplate.boundHashOps(KEY).values();
-        for (Object object : values) {
-            list.add((Session) object);
-        }
-        return list;
-    }
+//    @Autowired
+//    private RedisTemplate<String, Session> redisTemplate;
+//
+//    private static final String KEY = "shareSessionMap";
+//
+//    public void hadd(String sessionId, byte[] bytes){
+//        redisTemplate.boundHashOps(KEY).put(sessionId, bytes);
+//    }
+//    public void hadd(String sessionId, Session session){
+//        redisTemplate.boundHashOps(KEY).put(sessionId, session);
+//    }
+//
+//    public void hdelete(String sessionId){
+//        redisTemplate.boundHashOps(KEY).delete(sessionId);
+//    }
+//
+//    public Session hget(String sessionId){
+//        return (Session) redisTemplate.boundHashOps(KEY).get(sessionId);
+//    }
+//
+//    public List<Session> hmget(){
+//        List<Session> list = new ArrayList<>();
+//
+//        List<Object> values = redisTemplate.boundHashOps(KEY).values();
+//        for (Object object : values) {
+//            list.add((Session) object);
+//        }
+//        return list;
+//    }
 }
